4ed5263be6501aec6cdc1660e46595b364839b07
Moubien Kayali <mobeen-k@hotmail.com>
2020-05-21 01:10:44 +0300

    now

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -45,7 +45,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("PC says no!");
+            // throw new Exception("PC says no!");
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
