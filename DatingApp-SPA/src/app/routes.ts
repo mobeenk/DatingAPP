0df530614f5c30875bd725e99babce061bfb551a
Moubien Kayali <mobeen-k@hotmail.com>
2020-05-25 20:57:58 +0300

    section7

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,26 @@
+
+
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    // '' empty if we call main link http://localhost:4200/
+    { path: '', component: HomeComponent },
+    {
+         // if path:'main' this means the links down will be  http://localhost:4200/members
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
