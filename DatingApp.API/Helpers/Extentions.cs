b451add5780da7e334ef5af79990ed196fad3404
Moubien Kayali <mobeen-k@hotmail.com>
2020-08-25 01:33:30 +0300

    ggg

--- DatingApp.API/Helpers/Extentions.cs
+++ DatingApp.API/Helpers/Extentions.cs
@@ -14,6 +14,18 @@ namespace DatingApp.API.Helpers
 
 
         }
+        
+        // public static void AddPagination(this HttpResponse response, 
+        //     int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        // {
+        //     var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+        //     var camelCaseFormatter = new JsonSerializerSettings();
+        //     camelCaseFormatter.ContractResolver = 
+        //         new CamelCasePropertyNamesContractResolver();
+        //     response.Headers.Add("Pagination",
+        //         JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+        //     response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        // }
         public static int CalculateAge(this DateTime theDateTime)
         {
             var age = DateTime.Today.Year - theDateTime.Year;
