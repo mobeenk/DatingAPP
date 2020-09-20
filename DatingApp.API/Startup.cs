62ee6950abb491276ce67e6346ece5e94a40d5f9
Moubien Kayali <mobeen-k@hotmail.com>
2020-09-20 11:54:05 +0300

    publish

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -97,6 +97,7 @@ namespace DatingApp.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index","Fallback");
             });
         }
     }
